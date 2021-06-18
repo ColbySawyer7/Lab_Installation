d050b3a9ee9c302f033e40e21941a0bd0f93add6
ColbySawyer7 <sawyerc17@students.ecu.edu>
2021-06-18 13:11:27 -0400

    Added OpenNSA dependencies

--- install.py
+++ install.py
@@ -14,14 +14,23 @@ def update():
     stanout = subprocess.run(['git', 'pull'])
     if stanout.stdout is not None:
         print(stanout.stdout)
+    print("Update Complete!")
+    
+# Pip Dependency Helper func
+def pip_install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
+# Standard Package Installer 
+# **package must be String[]
+def install(package):
+    stanout = subprocess.run(['sudo', 'apt', 'install', package] )
+    if stanout.stdout is not None:
+        print(stanout.stdout)
 
 #OpenVPN
 if args.vpn:
     print('Installing OpenVPN...\n\nThis may take a minute, Please wait for entire process to complete\n')
-    stanout = subprocess.run(['sudo', 'apt', 'install', 'openvpn', 'easy-rsa'] )
-    if stanout.stdout is not None:
-        print(stanout.stdout)
+    install(['openvpn', 'easy-rsa'])
     print("Installation Complete!")
 #OpenNSA
 if args.nsa:
@@ -32,6 +41,21 @@ if args.nsa:
     if stanout.stdout is not None:
         print(stanout.stdout)
     os.chdir('opennsa')
+    # Install Dependencies    
+    print('Installing OpenNSA Dependencies...\n\n')
+    #Twisted Install
+    pip_install('twisted[tls]')
+    #Psycopg Install
+    install(['python3-dev', 'libpq-dev'])
+    pip_install('psycopg2')
+    #Twistar Install
+    pip_install('twistar')
+    #PostGreSQL Install
+    install(['postgresql'])
+    #pyOPenSSL Install
+    pip_install('pyOpenSSL')
+
+    #Navigate back to Lab Installation dir 
     os.chdir('..')
     os.chdir('Lab_Installation')
     print("Installation Complete!")
