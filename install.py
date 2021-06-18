13696a8b97b5194b8d10a8ab68d16b3cec5b67d9
ColbySawyer7 <sawyerc17@students.ecu.edu>
2021-06-18 12:18:40 -0400

    Initial Commit

---
+++ install.py
@@ -0,0 +1,25 @@
+#Installation Script for BRIDGES Lab
+#Prereqg: Git
+import argparse, subprocess
+# Menu 
+parser = argparse.ArgumentParser(description="Welcome to the BRIDGES Installation Helper Script")
+options = parser.add_mutually_exclusive_group()
+parser_vpn = options.add_argument('-v', '--vpn', action='store_true', help='Install OpenVPN and its dependencies')
+parser_nsa = options.add_argument('-n','--nsa', action='store_true', help='Install OpenNSA and its dependencies')
+args = parser.parse_args()
+
+#OpenVPN
+if args.vpn:
+    print('Installing OpenVPN...\n\nThis may take a minute, Please wait for entire process to complete')
+    stanout = subprocess.run(['sudo', 'apt', 'install', 'openvpn', 'easy-rsa', 'y'] )
+    print(stanout.stdout)
+    print("Installation Complete!")
+#OpenNSA
+if args.nsa:
+    print('Installing OpenNSA...\n\nThis may take a minute, Please wait for entire process to complete')
+    repoURL = 'https://github.com/NORDUnet/opennsa.git'
+    stanout = subprocess.run(['git', 'pull', repoURL])
+    print(stanout.stdout)
+    stanout = subprocess.run(['ls', 'opennsa'])
+    print("Installation Complete!")
+
