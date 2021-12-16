b68392c91d326822f28e19ef2813e7ea7aa951c5
ColbySawyer7 <sawyerc17@students.ecu.edu>
2021-12-16 13:51:43 -0500

    Update br_install.py

--- br_install.py
+++ br_install.py
@@ -87,10 +87,10 @@ def setup_opennsa(setup_db=False):
                 try:
                     cursor.execute(command)
                 except DuplicateObject as e:
-                    cursor.rollback()
+                    conn.rollback()
                     print(str(e) + 'Already Created')
                 except DuplicateDatabase as e:
-                    cursor.rollback()
+                    conn.rollback()
                     print('Database ' + str(e) + ' Already Created')
                 else:
                     conn.commit()
