b46a761e099f715c722760ee187e0b5c56ee0a46
ColbySawyer7 <sawyerc17@students.ecu.edu>
2021-12-16 10:13:09 -0500

    Update br_install.py

--- br_install.py
+++ br_install.py
@@ -2,7 +2,7 @@
 #Note: OpenNSA is installed in the working dir parent. We dont not yet support specifying dir for OpenNSA
 #Prereqg: Git
 #-h or --help for assistance
-import argparse, subprocess, os, sys, pwd
+import argparse, subprocess, os, sys, pwd, grp
 from constants import db_user, db_name
 
 if not (sys.version_info.major == 3 and sys.version_info.minor >= 0):
@@ -176,9 +176,10 @@ if args.nsa:
             stanout = subprocess.run(command)
             if stanout.stdout is not None:
                 print(stanout.stdout)
-    # Change ownership for certs to Opennsa user only
+    # Change ownership for certs to Opennsa user only.
+    gid = grp.getgrnam("nogroup").gr_gid
     uid = pwd.getpwnam("opennsa").pw_uid
-    os.chown('/keys', uid)
+    os.chown('/keys', uid, gid)
 
     reply = str(input('\nWould you like to run an instance of OpenNSA now?' +' (y/n): ')).lower().strip()
     if reply[0] == 'y':
