52aa43c808f81568422c318a12abbd8eec577531
ColbySawyer7 <sawyerc17@students.ecu.edu>
2022-01-07 15:09:10 -0500

    Created Executable

--- constants.py
+++ constants.py
@@ -1,10 +1,17 @@
-#Database Authentication
+# Constants File
+# Usage: 
+#   User can define instance-specific information for the various installation options
+
+#OpenNSA Database Authentication
 db_user='opennsa'
 db_name='opennsa'
 db_password='opennsa'
 
+#Applications Directory path. All of the sources will be installed in this location
+#This should adhere to industry standard. Unlikely this will need to be changed
+apps_dir='/usr/local'
+
+#Path to opennsa schema file. This file is supplied by OpenNSA source.
 #DEFAULT: THIS YOU SHOULDNT HAVE TO CHANGE
 default_path='opennsa/datafiles/schema.sql'
 
-#This should adhere to industry standard. Unlikely this will need to be changed
-apps_dir='/usr/local'
