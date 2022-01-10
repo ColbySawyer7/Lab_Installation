8be3b2e6a4fba41888b176da089d8afa79241f13
ColbySawyer7 <sawyerc17@students.ecu.edu>
2022-01-10 15:27:11 -0500

    Added Dependency Check

--- utils/gui.py
+++ utils/gui.py
@@ -1,11 +1,42 @@
 import PySimpleGUI as sg
 from toolkit import *
-from os import path
+from os import path, times_result
 
 SETTINGS_KEYS_TO_ELEMENT_KEYS = {'apps_dir': '-APPS DIR-', 'db_user': '-DB USER-', 'db_name': '-DB NAME-', 'db_password': '-DB PSWD-', 'default_path': '-PATH-', 'theme': '-THEME-'}
-from constants import SETTINGS_FILE_LOCATION
+from constants import SETTINGS_FILE_LOCATION, GREEN_BUTTON_COLOR, DARK_GRAY_BUTTON_COLOR, LIGHT_GRAY_BUTTON_COLOR, BLUE_BUTTON_COLOR, GREEN_CHECK_ICON, WARNING_ICON, RED_X_ICON
 
 settings = sg.UserSettings(SETTINGS_FILE_LOCATION, use_config_file=True, convert_bools_and_none=True)
+
+def dependency_window():
+    sg.theme(settings['Main']['theme'])
+    status = ''
+
+    selected_icon = WARNING_ICON
+    if status == 'comp':
+        selected_icon = GREEN_CHECK_ICON
+        status_message = "Depedency is in compliance"
+    elif status == 'noncomp':
+        selected_icon = RED_X_ICON
+        status_message = "ERROR: dependency is in NOT compliance; this will prevent the application from running"
+    else:
+        selected_icon = WARNING_ICON
+        status_message = "Unable to determine compliance of dependency at this time"
+
+    layout = [  [sg.Text('Dependencies', font='Any 15')],
+                [sg.Text('PostGreSQL 12.0+', font = 'Any 12') ,sg.Button('', image_data=selected_icon, button_color=(sg.theme_background_color(),sg.theme_background_color()), border_width=0)],
+                [sg.Text('', key='-STATUS-', font='Any 10')],
+                [sg.Button('Exit')]]
+
+    window = sg.Window('Dependency Helper', layout,size=(400,175), keep_on_top=True, finalize=True)
+
+    while True:             # Event Loop
+        event, values = window.read(timeout=100)
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            break
+        elif event == '':
+            window['-STATUS-'].update(value=status_message)
+    
+    window.close()
 
 def settings_window():
     sg.theme(settings['Main']['theme'])
@@ -35,7 +66,7 @@ def settings_window():
 
     while True:
         event, fields = window.read()
-        if event == 'Exit':
+        if event == sg.WIN_CLOSED or event == 'Exit':
             break
         elif event == 'Save':
             try:
@@ -58,18 +89,16 @@ def settings_window():
 
 def start_gui():
     sg.theme(settings['Main']['theme'])
-    BLUE_BUTTON_COLOR = '#FFFFFF on #2196f2'
-    GREEN_BUTTON_COLOR ='#FFFFFF on #00c851'
-    LIGHT_GRAY_BUTTON_COLOR = f'#212021 on #e0e0e0'
-    DARK_GRAY_BUTTON_COLOR = '#e0e0e0 on #212021'
 
-    layout = [[sg.Col([[sg.B('OpenNSA Installation', size=(40,2), button_color=BLUE_BUTTON_COLOR)],
-                        #[sg.B('OpenNSA Automatic Configuration', size=(40,2), button_color=BLUE_BUTTON_COLOR)],
-                        [sg.B('OpenVPN Installation and Configuration', size=(40,2), button_color=BLUE_BUTTON_COLOR)],
-                        [sg.B('GVS Installation', size=(40,2), button_color=BLUE_BUTTON_COLOR)],
-                        [sg.B('Update', size=(40,2), button_color=GREEN_BUTTON_COLOR)],
-                        [sg.B('Settings', size=(40,2), button_color=DARK_GRAY_BUTTON_COLOR)],
-                        [sg.B('Exit', size=(40,2), button_color=LIGHT_GRAY_BUTTON_COLOR)],], element_justification='c', vertical_alignment='c', expand_x=True,expand_y=True)]]
+    layout = [[sg.Text('Main Menu', font='Any 15')],
+            [sg.Col([[sg.B('OpenNSA Installation', size=(40,2))],
+            #[sg.B('OpenNSA Automatic Configuration', size=(40,2), button_color=BLUE_BUTTON_COLOR)],
+            [sg.B('OpenVPN Installation and Configuration', size=(40,2))],
+            [sg.B('GVS Installation', size=(40,2))],
+            [sg.B('Dependency Helper', size=(40,2), button_color=BLUE_BUTTON_COLOR)],
+            [sg.B('Update', size=(40,2), button_color=GREEN_BUTTON_COLOR)],
+            [sg.B('Settings', size=(40,2), button_color=DARK_GRAY_BUTTON_COLOR)],
+            [sg.B('Exit', size=(40,2), button_color=LIGHT_GRAY_BUTTON_COLOR)],], element_justification='c', vertical_alignment='c', expand_x=True,expand_y=True)]]
 
     window = sg.Window('BRIDGES Lab Installation', layout, resizable=True, size=(400,400), icon='assets/bridges.jpg')
 
@@ -77,8 +106,8 @@ def start_gui():
         event, values = window.read(timeout=100)
         if event == sg.WIN_CLOSED or event == 'Exit':
             break
-        elif event == 'Do Something':
-            window['-GIF-'].metadata = 0
+        elif event == 'Dependency Helper':
+            dependency_window()
         elif event == 'OpenNSA Installation':
             configure_opennsa()
         elif event == 'OpenVPN Installation and Configuration':
@@ -95,4 +124,4 @@ def start_gui():
                 start_gui()
 
 # TESTING HELPER (UNCOMMENT TO TEST GUI ONLY)
-# start_gui()
+#start_gui()
